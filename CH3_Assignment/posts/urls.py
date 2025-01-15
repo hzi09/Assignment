6395c8dc8f4e264c5b9045d0915072e5b47d56d3
Lee Hyunji <hzi284914@gmail.com>
2025-01-15 17:42:11 +0900

    add create posts

--- CH3_Assignment/posts/urls.py
+++ CH3_Assignment/posts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:pk>', views.post_detail, name='post_detail'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('create/',views.post_create, name='post_create'),
+    path('<int:pk>/update/', views.post_update, name='post_update'),
 ]
--- CH3_Assignment/posts/views.py
+++ CH3_Assignment/posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import PostForm
 from .models import Post
 
 def post_list(request) :
@@ -8,9 +9,42 @@ def post_list(request) :
     }
     return render(request, 'posts/post_list.html', context)
 
+
 def post_detail(requset, pk) :
     post = get_object_or_404(Post, pk=pk)
     context = {
         "post" : post,
     }
     return render(requset, 'posts/post_detail.html', context)
+
+
+
+def post_create(request):
+    if request.method == 'POST' :
+        form = PostForm(request.POST)
+        if form.is_valid() :
+            post = form.save()
+            return redirect('posts:post_detail', post.pk)
+    else :
+        form = PostForm
+
+    context = {
+        'form' : form,
+    }
+    return render(request, 'posts/post_form.html', context)
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('posts:post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    
+    context = {
+        'form': form,
+        'post': post,
+    }
+    return render(request, 'posts/post_form.html', context)
