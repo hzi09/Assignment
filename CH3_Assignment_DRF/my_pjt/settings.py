88c1e2d330533d602104605cd71f437154c85a7a
Lee Hyunji <hzi284914@gmail.com>
2025-01-23 10:42:39 +0900

    migrate MySQL

--- CH3_Assignment_DRF/my_pjt/settings.py
+++ CH3_Assignment_DRF/my_pjt/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from datetime import timedelta
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,10 +83,29 @@ WSGI_APPLICATION = 'my_pjt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'USER' : 'root',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+        'OPTIONS': {
+            # MySQL의 데이터 무결성을 보장
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            # 이모지 및 다양한 유니코드 문자를 저장할 수 있도록 보장
+            'charset': 'utf8mb4', 
+            # Django와 MySQL 간 유니코드 데이터 처리를 원활하게 함
+            'use_unicode': True,
+        },
     }
 }
 
